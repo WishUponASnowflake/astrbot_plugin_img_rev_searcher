d6a4002f9c1a565a4fdaa9a29951f5b18ee34b84
drdon1234 <96719161+drdon1234@users.noreply.github.com>
2025-07-28 19:41:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def get_message_text(message) -> str:
     return ''
 
 
-@register("astrbot_plugin_img_rev_seacher", "drdon1234", "以图搜图，找出处", "2.3")
+@register("astrbot_plugin_img_rev_searcher", "drdon1234", "以图搜图，找出处", "2.3")
 class ImgRevSearcherPlugin(Star):
     """
     以图搜图插件主类
